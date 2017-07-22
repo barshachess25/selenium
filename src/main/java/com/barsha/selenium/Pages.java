b357daf931c09ca9e84c7efad8f6a2d05fe21689
Sengupta <prasengupta@deloitte.com>
2017-07-22 19:42:36 +0530

    added pages class

---
+++ src/main/java/com/barsha/selenium/Pages.java
@@ -0,0 +1,7 @@
+package com.barsha.selenium;
+
+import com.barsha.selenium.test.TestBaseSetup;
+
+public class Pages {
+	public static LoginPage loginPage = TestBaseSetup.initPage(LoginPage.class);
+}
